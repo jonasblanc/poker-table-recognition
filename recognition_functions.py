24efb18ac74e0a11e8e984d02cd8339f1c066981
Jonas Blanc <jonas.blanc@epfl.ch>
2022-05-06 21:57:24 +0200

    Implement first table extraction with lines

--- recognition_functions.py
+++ recognition_functions.py
@@ -18,17 +18,22 @@ def rectify(h):
 
 def table_extraction(img, table_size=3800):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(1,1),1000) 
+    blur = cv2.GaussianBlur(gray,(11,11),1000) 
     flag, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_OTSU)
     
-    closed = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel= np.ones((11,11), np.uint8))
+    opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel= np.ones((51,51), np.uint8))
 
-    contours, hierarchy = cv2.findContours(closed,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(opened,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=cv2.contourArea,reverse=True) 
     
     table_cont = contours[0]
+    
+    #rot_rect = cv2.minAreaRect(contours[0])
+    #box = cv2.boxPoints(rot_rect)
     peri = cv2.arcLength(table_cont,True)
-    approx = rectify(cv2.approxPolyDP(table_cont,0.02*peri,True))
+    approx = np.array((cv2.approxPolyDP(table_cont,0.02*peri,True)))[:,0]
+    diff = np.abs(approx[:,0]-approx[:,1])
+    approx = rectify(approx[np.argsort(diff)[::-1][:4]])
 
     h = np.array([ [0,0],[table_size,0],[table_size,table_size],[0,table_size] ],np.float32)
     transform = cv2.getPerspectiveTransform(approx, h)
